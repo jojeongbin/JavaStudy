003b3c5cb0fd490acb959f1737c9fd5156484586
jojeongbin <cjb1009@naver.com>
2023-03-07 00:36:21 +0900

    매개변수를 사용해서 전사가 매번 다르게 공격하도록하기, 마지막 공격방식을 기억 ver1

--- weapon.java
+++ weapon.java
@@ -1,39 +1,40 @@
 // 문제 : 매개변수를 사용해서 전사가 매번 다르게 공격하도록 해주세요.
 
 class Main {
-  public static void main(String[] args) {
-    전사 a전사 = new 전사();
+	public static void main(String[] args) {
+		전사 a전사 = new 전사();
 
-    a전사.공격("브라이언" , "칼"); 
-    // 브라이언이(가) 칼(으)로 공격합니다.
+		a전사.공격("브라이언", "칼");
+		// 브라이언이(가) 칼(으)로 공격합니다.
 
-    a전사.재공격();
-    // 브라이언이(가) 칼(으)로 공격합니다.
+		a전사.재공격();
+		// 브라이언이(가) 칼(으)로 공격합니다.
 
-    a전사.공격("필립","창"); 
-    // 필립이(가) 창(으)로 공격합니다.
+		a전사.공격("필립", "창");
+		// 필립이(가) 창(으)로 공격합니다.
 
-    a전사.공격("마크","지팡이"); 
-    // 마크(가) 지팡이(으)로 공격합니다.
+		a전사.공격("마크", "지팡이");
+		// 마크(가) 지팡이(으)로 공격합니다.
 
-    a전사.재공격();
-    // 마크(가) 지팡이(으)로 공격합니다.
+		a전사.재공격();
+		// 마크(가) 지팡이(으)로 공격합니다.
 
-    a전사.재공격();
-    // 마크(가) 지팡이(으)로 공격합니다.
-  }
+		a전사.재공격();
+		// 마크(가) 지팡이(으)로 공격합니다.
+	}
 }
-
 class 전사 {
-  String lastName;
-  String lastWeaponName;
-  void 공격(String name, String weapon){
-    System.out.println(name+"이(가)"+weapon+"(으)로 공격합니다.");
-    lastName = name;
-    lastWeaponName = weapon;
-  
-  }
-  void 재공격(){
-    System.out.println(lastName+"이(가)"+lastWeaponName+"(으)로 공격합니다.");
-  }
+	String 공격자명;
+	String 무기명;
+
+	void 공격(String 공격자명, String 무기명) {
+		this.공격자명 = 공격자명;
+		this.무기명 = 무기명;
+		
+		재공격();
+	}
+
+	void 재공격() {
+		System.out.printf("%s(이)가 %s(으)로 공격합니다!\n", 공격자명, 무기명);
+	}
 }
